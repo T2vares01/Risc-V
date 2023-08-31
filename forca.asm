091c096a8c7acee68fe701f0f86d8ec8d912cca4
T2vares01 <aluno.lucas.tavares@cmdpii.com.br>
2023-08-31 15:39:22 -0300

    feat: Funcionando, sem condicao de vitoria,
    falta repeticoes de letra

--- forca.asm
+++ forca.asm
@@ -1,5 +1,5 @@
 .eqv STRING 4
-.eqv COMPRIMENTO_NOME 8
+.eqv COMPRIMENTO_NOME 7
 
 .macro print_str (%str)
 .data
@@ -17,13 +17,13 @@ espaço : .string
 .text
 
 print_str("VIDAS ")
-
 li s0, 6  # VIDAS do usario
 mv a0, s0
 li a7, 1  # printa
 ecall
 
 print_str("\n    ")
+
 
 la a0,nome_secreto # a0 = nome_secreto
 li a7,4    # a7 = 1 # print integer cmd
@@ -34,6 +34,11 @@ print_str("\n")
  #li a7,12   # capta o char
 #ecall
 game_loop:
+print_str(" \n ")
+la t5, nome_secreto
+li t4, 0
+li t3, -1
+mv zero, t4
 la t0, nome    # endereço inicial
 addi t2, t0, COMPRIMENTO_NOME # endereço final
 li a7,12   # capta o char
@@ -44,6 +49,7 @@ loop:
 	beq t1, a0, venceu
 	
 	addi t0, t0, 1
+	addi t4, t4, 1
 	j loop
 perde: print_str(" \n smt ") #caso não tenha 
 	addi s0 , s0 ,-1 # adicionar -1
@@ -54,16 +60,21 @@ perde: print_str(" \n smt ") #caso não tenha
 	
 	j game_loop
 venceu: 
-	li t3, 0
-	print_str(" tem")
-	bge t0, t2, game_loop
+	# t0 = endereco da letra, t1= letra t2= enderoco final, t3 = loop controle, t4= pos da letra atual
+	#print_str(" tem")
 	escreve_palavra:
-	beq t3, t0, imprime_letra# se a posicao da letra
-	print_str("_ ")
 	addi t3, t3, 1 #
+	li a0, COMPRIMENTO_NOME
+	bge t3, a0, game_loop
+	beq t3, t4, imprime_letra# se a posicao da letra
 	j escreve_palavra
 	imprime_letra: 
-	mv a0, t1# t1 = letra
+	li a1, 2
+	mul a1, a1, t3#dobro de pos
+	add a0, a1, t5
+	sb t1, 0(a0)
+	
+	la a0, nome_secreto
 	li a7, 4
 	ecall
 	j venceu
